62c6bb9960ecd73a4ae69d020a2a4de921fa27a0
CritianMendivelso <critian.mendivelso@escuelaing.edu.co>
2017-04-01 17:23:39 -0500

    archivos basicos

---
+++ src/main/java/edu/eci/arsw/pacm/pacm.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.pacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pacm {
+
+	public static void main(String[] args) {
+		SpringApplication.run(pacm.class, args);
+	}
+}
