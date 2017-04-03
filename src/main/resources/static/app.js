bf09d12e6997a7c4dcb914a7ef51d2b0fd0b6c3e
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-04-03 18:41:57 -0500

    tablero cargado

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,6 @@
 var stompClient = null;
 
+
 function connect() {
     var socket = new SockJS('/stompendpoint');
     stompClient = Stomp.over(socket);
@@ -19,10 +20,57 @@ function connect() {
                             
                         }
                         else if (tablero[i][j] === "1"){
-                            var myObstacle = new circle(20, 20, "red", (j * 20) +10, (i * 20) +10);
+                            var myObstacle = new circle(3,20, 20, "white", (j * 20) +10, (i * 20) +10);
                             myObstacle.update();
                             
                         }
+                        else if (tablero[i][j] === "2"){
+                            var myObstacle = new circle(5,20, 20, "white", (j * 20) +10, (i * 20) +10);
+                            myObstacle.update();
+                            
+                        }
+                        
+                        else if (tablero[i][j]==="A" ){
+                            console.log(tablero[i][j]);
+                            console.log(tablero[i][j]==="A" );
+                            var myObstacle = new pacman(20, 20, "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/2000px-Pacman.svg.png", j * 20,  i * 20, "image");
+                            jugadorpacman=myObstacle;
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="B" ){
+                            var myObstacle = new pacman(20, 20, "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/2000px-Pacman.svg.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="C" ){
+                            var myObstacle = new pacman(20, 20, "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/2000px-Pacman.svg.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="D" ){
+                            var myObstacle = new pacman(20, 20, "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/2000px-Pacman.svg.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        
+                        else if (tablero[i][j]==="a" ){
+                            var myObstacle = new ghost(20, 20, "https://static.giantbomb.com/uploads/scale_small/8/87790/2469743-orange.png", j * 20,  i * 20, "image");
+                            jugadorfantasma=myObstacle;
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="b" ){
+                            var myObstacle = new ghost(20, 20, "https://static.giantbomb.com/uploads/scale_small/8/87790/2469743-orange.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="c" ){
+                            var myObstacle = new ghost(20, 20, "https://static.giantbomb.com/uploads/scale_small/8/87790/2469743-orange.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        else if (tablero[i][j]==="d" ){
+                            var myObstacle = new ghost(20, 20, "https://static.giantbomb.com/uploads/scale_small/8/87790/2469743-orange.png", j * 20,  i * 20, "image");
+                            myObstacle.update();
+                        }
+                        
+                                                 
+                        
+                        
                     }
                 }
             }
@@ -33,6 +81,106 @@ function connect() {
 }
 
 
+function ghost(width, height, color, x, y, type) {
+    this.type = type;
+    if (type === "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        //var canvas = document.getElementById('cnv');
+        //var ctx = canvas.getContext('2d');
+        if (type === "image") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+    this.crashRight = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crashleft = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crashleft = false;
+        }
+        return crashleft;
+    }
+}
+
+
+
+function pacman(width, height, color, x, y, type) {
+    this.type = type;
+    if (type === "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        //var canvas = document.getElementById('cnv');
+        //var ctx = canvas.getContext('2d');
+        if (type === "image") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+    this.crashRight = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crashleft = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crashleft = false;
+        }
+        return crashleft;
+    }
+}
+
+function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
+
+
+
 function bloque(width, height, color, x, y) {
     this.width = width;
     this.height = height;
@@ -41,8 +189,8 @@ function bloque(width, height, color, x, y) {
     this.x = x;
     this.y = y;
     this.update = function () {
-        var canvas = document.getElementById('cnv');
-        var ctx = canvas.getContext('2d');
+        //var canvas = document.getElementById('cnv');
+        //var ctx = canvas.getContext('2d');
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
@@ -66,17 +214,17 @@ function bloque(width, height, color, x, y) {
     }
 }
 
-function circle(width, height, color, x, y) {
+function circle(radio,width, height, color, x, y) {
+    this.radio=radio;
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
     this.update = function () {
-        var canvas = document.getElementById('cnv');
-        var ctx = canvas.getContext('2d');
+        
         ctx.fillStyle = color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI);
+        ctx.arc(this.x, this.y, radio, 0, 2 * Math.PI);
         ctx.fill();
             
 
@@ -121,16 +269,34 @@ function disconnect() {
     console.log("Disconnected");
 }
 
+function controlarpacman(){
+    myplayer=jugadorpacman;
+}
 
+
+function controlarfantasma(){
+    myplayer=jugadorfantasma;
+}
 
 
 $(document).ready(
         function () {
             console.info('loading script!...');
             connect();
-
+            canvas = document.getElementById('cnv');
+            ctx = canvas.getContext('2d');
+            
+            window.addEventListener('keydown', function (e) {
+            key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            key = false;
+        })
+           
         }
 );
+
+
 
 
 
