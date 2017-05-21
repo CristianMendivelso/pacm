37b9f725f48bdf0d87c51afb6f437da84eaa93aa
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-05-21 16:32:29 -0500

    arreglo nombres

--- src/main/resources/static/entrar.js
+++ src/main/resources/static/entrar.js
@@ -3,9 +3,10 @@ var username;
 function guardar() {
     username = document.getElementById('username').value;
 
-    if (username === "") {
-        alert("Ingrese Un Nombre De Usuario");
+    if (username==="" || username.length>9) {
+        alert("Ingrese Un Username Válido");
     }
+    
     else {
         sessionStorage.setItem('username', document.getElementById('username').value);
         document.location.href = "select.html";
